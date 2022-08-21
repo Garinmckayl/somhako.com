3882869d6153c6453508f92b3baa17a2f01ad75b
Natnael <natnaelgetenew@gmail.com>
2022-08-21 17:58:31 +0300

    ew

--- components/footer.js
+++ components/footer.js
@@ -93,14 +93,7 @@ export default function Footer() {
         </div>
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
-          <a href="https://web3templates.com/" target="_blank" rel="noopener">
-            Web3Templates.
-          </a>{" "}
-          Illustrations from{" "}
-          <a href="https://www.glazestock.com/" target="_blank" rel="noopener ">
-            Glazestock
-          </a>
+          Copyright © {new Date().getFullYear()}.Somhako
         </div>
       </Container>
     </div>
