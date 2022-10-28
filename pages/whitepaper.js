b644f2bca1a3900608e8136183687f25962c5f8b
anmol-somhako <anmol.verma@somhako.com>
2022-10-28 14:13:46 +0530

    minor design change

--- pages/whitepaper.js
+++ pages/whitepaper.js
@@ -13,13 +13,14 @@ export default function Whitepaper() {
         />
       </Head>
           <div className="py-10">
-              <div className="container mx-auto">
+              <div className="container mx-auto px-3">
                   <h1 className="mb-5 text-3xl text-center lg:text-4xl tracking-tight font-extrabold text-white">
                   Whitepaper
                   </h1>
-                  <div className="max-w-[320px] md:max-w-[360px] mx-auto relative overflow-hidden pb-[100%] md:pb-[66%] lg:pb-[40%]">
-                    <iframe src="https://www.canva.com/design/DAFNJ1GIOKo/view?embed" className="h-full w-full absolute left-0 top-0" allowfullscreen="allowfullscreen" allow="fullscreen">
+                  <div className="max-w-[320px] md:max-w-[360px] mx-auto relative overflow-hidden text-center">
+                    <iframe src="https://www.canva.com/design/DAFNJ1GIOKo/view?embed" className="h-[450px] md:h-[510px] w-full mb-4" allowfullscreen="allowfullscreen" allow="fullscreen">
                     </iframe>
+                    <a href="https://www.canva.com/design/DAFNJ1GIOKo/view" target="_blank" className="inline-block py-2 px-4 text-sm rounded font-bold text-white bg-gradient-to-r from-purple-500 to-blue-500 hover:from-blue-500 hover:to-purple-500">Open it in new tab</a>
                   </div>
               </div>
           </div>
